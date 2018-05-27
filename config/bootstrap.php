17aeddc2f56b29d04d4528c823705858bf78b32b
Amitav Roy <reachme@amitavroy.com>
2018-05-27 10:12:59 +0530

    first commit

---
+++ config/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /**
+     * This it the role which admins will get by default.
+     * A user with this role will have all the permissions.
+     */
+    'admin' => 'administrator',
+
+    /**
+     * This is the role which any registered user will get
+     * as soon as the user registers.
+     */
+    'authenticated' => 'authenticated',
+
+];
---
+++ database/seeds/RoleSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Spatie\Permission\Models\Role::create(['name' => 'administrator']);
+        \Spatie\Permission\Models\Role::create(['name' => 'authenticated']);
+    }
+}
